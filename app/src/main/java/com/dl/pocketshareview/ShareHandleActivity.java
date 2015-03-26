f1a32b9cb6e62d0f04ac26f377782c513130e1c7
logicmelody <logicmelody@gmail.com>
2015-03-26 11:51:32 +0800

    Revert "When receive the shared action, the activity will not show"
    This reverts commit 4aaf986d04fd352e079816b5e2fd89a93b8bce4c.

--- app/src/main/java/com/dl/pocketshareview/ShareHandleActivity.java
+++ app/src/main/java/com/dl/pocketshareview/ShareHandleActivity.java
@@ -3,7 +3,6 @@ package com.dl.pocketshareview;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
 
 /**
  * Created by logicmelody on 15/3/25.
@@ -13,7 +12,6 @@ public class ShareHandleActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(this, "Got shared action", Toast.LENGTH_SHORT).show();
         startService(new Intent(this, FloatingViewService.class));
         finish();
     }
