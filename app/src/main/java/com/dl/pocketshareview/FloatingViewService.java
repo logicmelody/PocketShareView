64695e804c38abb1a996c2cd1e31959a996c3a24
logicmelody <logicmelody@gmail.com>
2015-06-02 16:46:00 +0800

    TYPE_TOAST

--- app/src/main/java/com/dl/pocketshareview/FloatingViewService.java
+++ app/src/main/java/com/dl/pocketshareview/FloatingViewService.java
@@ -99,7 +99,7 @@ public class FloatingViewService extends Service {
         WindowManager.LayoutParams params = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+                WindowManager.LayoutParams.TYPE_TOAST,
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |
                 WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
                 PixelFormat.TRANSLUCENT);
